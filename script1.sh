df9a297fa74793f6d6ec9559068e5846334ac0ab
Honey Sharma <113124234+honeyy02@users.noreply.github.com>
2024-07-21 11:54:33 +0000

    added new file

--- script1.sh
+++ script1.sh
@@ -4,9 +4,4 @@ read -p "Enter the dir to clone the git repo into:" dir
 mkdir "$dir"
 
 git clone "$repo_url" "$dir"
-
-if[ $? -eq 0 ];then
-  echo "Repo clone successfully into $dir"
-else
-  echo "failed to clone the repo"
-fi    
+  
